7eef226c89a75d1995224339c1e6d6d4ce9d7c0b
Adam Grare <adam@grare.com>
2022-12-15 16:45:40 -0500

    Remove duplicated ensure_network_manager code

--- app/models/manageiq/providers/redhat/infra_manager.rb
+++ app/models/manageiq/providers/redhat/infra_manager.rb
@@ -25,56 +25,16 @@ class ManageIQ::Providers::Redhat::InfraManager < ManageIQ::Providers::Ovirt::In
   require_nested  :DistributedVirtualSwitch
   include_concern :ApiIntegration
 
+  has_one :network_manager,
+          :foreign_key => :parent_ems_id,
+          :class_name  => "ManageIQ::Providers::Redhat::NetworkManager",
+          :autosave    => true,
+          :dependent   => :destroy
+
   supports :catalog
   supports :create
   supports :metrics
   supports :provisioning
-
-  supports :create_iso_datastore do
-    unsupported_reason_add(:create_iso_datastore, _("Already has an ISO datastore")) if iso_datastore
-  end
-
-  def ensure_managers
-    return unless enabled
-    ensure_network_manager
-    if network_manager
-      network_manager.name = "#{name} Network Manager"
-      network_manager.zone_id = zone_id
-      network_manager.provider_region = provider_region
-      network_manager.tenant_id = tenant_id
-      network_manager.save!
-    end
-  end
-
-  def ensure_network_manager
-    providers = ovirt_services.collect_external_network_providers
-
-    unless providers.blank?
-      providers = providers.sort_by(&:name)
-      auth_url = providers.first.authentication_url
-    end
-
-    if auth_url
-      if network_manager.nil?
-        ems_was_removed = false
-
-        if id # before update
-          ems = ExtManagementSystem.find_by(:id => id)
-          ems_was_removed = ems.nil? || !ems.enabled
-        end
-
-        unless ems_was_removed
-          build_network_manager(:type => 'ManageIQ::Providers::Redhat::NetworkManager')
-        end
-      end
-
-      if network_manager
-        populate_network_manager_connectivity(auth_url)
-      end
-    elsif network_manager
-      network_manager.destroy_queue
-    end
-  end
 
   def self.ems_type
     @ems_type ||= "rhevm".freeze
