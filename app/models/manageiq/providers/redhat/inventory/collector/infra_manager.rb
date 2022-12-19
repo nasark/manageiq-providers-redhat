b082612662c55dc37ff280d20d84ec3c710b849f
Adam Grare <adam@grare.com>
2022-12-19 10:53:58 -0500

    Cleanup Inventory::Collector::InfraManager

--- app/models/manageiq/providers/redhat/inventory/collector/infra_manager.rb
+++ app/models/manageiq/providers/redhat/inventory/collector/infra_manager.rb
@@ -1,8 +1,2 @@
 class ManageIQ::Providers::Redhat::Inventory::Collector::InfraManager < ManageIQ::Providers::Ovirt::Inventory::Collector::InfraManager
-  def collected_inventory
-    @collected_inventory ||= begin
-                               inventory_collector = ManageIQ::Providers::Redhat::InfraManager::Inventory.new(:ems => manager)
-                               inventory_collector.refresh
-                             end
-  end
 end
