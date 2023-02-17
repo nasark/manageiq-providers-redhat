5b2ff1ecdc6dd9818dc5fe90b050af517bf6ed8c
Adam Grare <adam@grare.com>
2023-02-17 10:39:08 -0500

    Remove OVN NetworkManager EventCatcher

--- app/models/manageiq/providers/redhat/network_manager/event_catcher.rb
+++
@@ -1,7 +0,0 @@
-class ManageIQ::Providers::Redhat::NetworkManager::EventCatcher < ::MiqEventCatcher
-  require_nested :Runner
-
-  def self.settings_name
-    :event_catcher_redhat_network
-  end
-end
